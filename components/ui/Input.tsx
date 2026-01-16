cb5d0ebe129dc3fd161aeefedfdc1f96acfaf6d1
mikelamann1@gmail.com <mikelamann1@gmail.com>
2026-01-16 12:36:31 +0100

    component input tilføjet

---
+++ components/ui/Input.tsx
@@ -0,0 +1,33 @@
+
+
+import { InputHTMLAttributes, forwardRef } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+  error?: string
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, className = '', ...props }, ref) => {
+    return (
+      <div className="w-full">
+        {label && (
+          <label className="block text-sm font-medium mb-2 dark:text-white">
+            {label}
+          </label>
+        )}
+        <input
+          ref={ref}
+          className={`input-underline ${className}`}
+          {...props}
+        />
+        {error && (
+          <p className="mt-1 text-sm text-red-500">{error}</p>
+        )}
+      </div>
+    )
+  }
+)
+
+Input.displayName = 'Input'
+export default Input
